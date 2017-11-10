c4ff848dea1e2c76733163a44cda0bec7d6cb7ea
Brian Davis <slimm609@gmail.com>
2017-11-09 19:41:25 -0500

    add alpine

--- build.sh
+++ build.sh
@@ -11,11 +11,13 @@ GOARCH='amd64'
 export GOPATH=$(pwd)
 
 go get
+#### THIS SECTION IS HERE UNTIL DEPS ARE MOVED TO THE NEW ETCD ####
 go get -u github.com/ugorji/go/codec/codecgen
 
 cd src/github.com/coreos/go-etcd/etcd
 $GOPATH/bin/codecgen -d 1978 -o response.generated.go response.go
 cd $GOPATH
+###################################################################
 
 for GOOS in "${OSES[@]}"; do
   echo "Building $GOOS-$GOARCH binary"
@@ -25,17 +27,17 @@ for GOOS in "${OSES[@]}"; do
     -v "$PWD:/go/src/$APP" -w "/go/src/$APP" \
     -e "GOOS=$GOOS" -e "GOARCH=$GOARCH" \
     -e "GOPATH=/go/src/$APP" \
-    golang:1.8 \
+    golang:1.9 \
        go build -v -ldflags "-X main.Version=$(cat VERSION)" -o "$PKGDIR/$GOOS/registrator"
 done
 
-#  echo "Building linux-alpine binary"
-#  echo "-----"
+  echo "Building linux-alpine binary"
+  echo "-----"
 
-#  docker run --rm \
-#      -v "$PWD:/go/src/$APP" -w "/go/src/$APP" \
-#      -e "GOOS=linux" -e "GOARCH=$GOARCH" \
-#      golang:1.9-alpine \
-#        go build -v -o "$PKGDIR/linux-alpine/summon-s3"
+  docker run --rm \
+      -v "$PWD:/go/src/$APP" -w "/go/src/$APP" \
+      -e "GOOS=linux" -e "GOARCH=$GOARCH" \
+      golang:1.9-alpine \
+        go build -v -o "$PKGDIR/linux-alpine/summon-s3"
 
 rm -rf bin/ src/
